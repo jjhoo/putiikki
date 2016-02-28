5ef3e127ad4f6c16da48c756290b9e1bb8270a64
Jani Hakala <jjhakala@gmail.com>
2016-02-28 13:00:08 +0200

    Initial DB design

--- models.py
+++ models.py
@@ -1,7 +1,76 @@
 import sqlalchemy as sqla
+from sqlalchemy import Column, Boolean, Integer, Numeric, String, \
+    CheckConstraint, ForeignKey, UniqueConstraint
+
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.engine.url import URL
+
+# varchar or text type? no truncation with text, common enough?
+from sqlalchemy.dialects.postgresql import TEXT
 
 import settings
 
+Base = declarative_base()
+
+class Catalog(Base):
+    __tablename__ = 'catalog'
+
+    id = Column(Integer, primary_key=True)
+    item_code = Column(TEXT, nullable=False, unique=True)
+    # descriptions could be in a separate table
+    # (language, short_description, description)
+    item_short_description = Column(TEXT, nullable=False)
+    item_description = Column(TEXT, nullable=False)
+
+    __table_args__ = (CheckConstraint('char_length(item_code) >= 4'),)
+
+class Stock(Base):
+    __tablename__ = 'stock'
+    id = Column(Integer, primary_key=True)
+    item = Column(Integer,
+                  ForeignKey("catalog.id",
+                             onupdate="CASCADE", ondelete="CASCADE"),
+                  nullable=False)
+    count = Column(Integer, nullable=False)
+    price = Column(Numeric(12,2), nullable=False)
+    visible = Column(Boolean, default=True, nullable=False)
+
+    __table_args__ = (CheckConstraint('count >= 0'),
+                      CheckConstraint('price >= 0.0'), )
+
+class Basket(Base):
+    __tablename__ = 'basket'
+    id = Column(Integer, primary_key=True)
+    item = Column(Integer,
+                  ForeignKey("catalog.id",
+                             onupdate="CASCADE", ondelete="CASCADE"),
+                  nullable=False)
+    # count may be larger then reserved count
+    count = Column(Integer, nullable=False)
+
+    __table_args__ = (CheckConstraint('count >= 0'),)
+
+class Reservations(Base):
+    __tablename__ = 'reservations'
+    id = Column(Integer, primary_key=True)
+    item = Column(Integer,
+                  ForeignKey("stock.id",
+                             onupdate="CASCADE", ondelete="CASCADE"),
+                  nullable=False)
+    basket = Column(Integer,
+                    ForeignKey("basket.id",
+                               onupdate="CASCADE", ondelete="CASCADE"),
+                    nullable=False)
+    count = Column(Integer, nullable=False)
+
+    __table_args__ = (CheckConstraint('count >= 0'),
+                      UniqueConstraint('item', 'basket'))
+
 def db_connect():
     return sqla.create_engine(URL(**settings.DB_ENGINE))
+
+def create_tables(engine):
+    Base.metadata.create_all(engine)
+
+def drop_tables(engine):
+    Base.metadata.drop_all(engine)
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 import models
 
-print(models.db_connect())
+dbc = models.db_connect()
+models.drop_tables(dbc)
+models.create_tables(dbc)
